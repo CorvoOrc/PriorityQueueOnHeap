3f05843906667b3b028857803e171bd187156467
Alexander Steshenko <CorvoOrc@users.noreply.github.com>
2015-01-24 23:56:25 +0400

    Update Heap.cpp
    Introduced separate compilation of templates

--- Heap.cpp
+++ Heap.cpp
@@ -1,95 +1,112 @@
 #include "Heap.h"
 
-//template <class T>
-//Heap::Heap(uint lenght_, T *store_, Type type_){
-//	this->heap_size = lenght_;
-//	this->lenght = lenght_;
-//
-//	this->store = new T[lenght];
-//	for (uint i = INDEX_ROOT; i <= lenght; ++i)
-//		this->store[i] = store_[i];
-//
-//	this->coef = type_ == nondecreasing ? 1 : -1;
-//
-//	build_heap();
-//}
+template <class T>
+Heap<T>::Heap(const uint &lenght_, const T *data_, const Type &type_) {
+	this->heap_size = lenght_;
+	this->lenght = lenght_;
 
-//template <class T>
-//Heap<T>::~Heap(){
-//}
+	try{
+		this->data = new T[lenght];
+	}
+	catch (std::exception e){
+		throw std::runtime_error("new data not allocated");
+	}
 
-//template <class T>
-//uint Heap<T>::parent(const uint &i) {
-//	return i >> 1;
-//}
-//
-//template <class T>
-//uint Heap<T>::left(const uint &i) {
-//	return i << 1;
-//}
-//
-//template <class T>
-//uint Heap<T>::right(const uint &i) {
-//	return (i << 1) + 1;
-//}
+	try{
+		memcpy(this->data, data_, sizeof(T) * (this->lenght + INDEX_ROOT));
+	}
+	catch (std::exception e){
+		throw std::runtime_error("memory not allocated");
+	}
 
-//template <class T>
-//void Heap<T>::heapify(const uint &i) {
-//	uint l = left(i);
-//	uint r = right(i);
-//
-//	uint largest = i;
-//
-//	if (l <= heap_size && store[l] * coef > store[i] * coef)
-//		largest = l;
-//	if (r <= heap_size && store[r] * coef > store[largest] * coef)
-//		largest = r;
-//
-//	if (largest != i) {
-//		std::swap(store[i], store[largest]);
-//		heapify(largest);
-//	}
-//}
+	this->coef = type_ == nondecreasing ? -1 : 1;
 
-//template <class T>
-//void Heap<T>::build_heap() {
-//	for (uint i = lenght/2; i >= INDEX_ROOT; --i)
-//		heapify(i);
-//}
-//
-//template <class T>
-//void Heap<T>::swap(const uint &i, const uint &j) {
-//	int tmp = this->store[i];
-//	this->store[i] = this->store[j];
-//	this->store[j] = tmp;
-//}
+	build_heap();
+}
 
-//template <class T>
-//void Heap<T>::decrease_size() {
-//	--this->heap_size;
-//}
-//
-//template <class T>
-//void Heap<T>::increase_size() {
-//	++this->heap_size;
-//}
+template <class T>
+Heap<T>::~Heap() {
+	delete[] data;
+}
 
-//template <class T>
-//uint Heap<T>::get_coef(){
-//	return this->coef;
-//}
+template <class T>
+uint Heap<T>::parent(const uint &i) {
+	return i >> 1;
+}
 
-//template <class T>
-//uint Heap<T>::get_size(){
-//	return this->heap_size;
-//}
+template <class T>
+uint Heap<T>::left(const uint &i) {
+	return i << 1;
+}
 
-//template <class T>
-//uint Heap<T>::get_lenght(){
-//	return this->lenght;
-//}
-//
-//template <class T>
-//const T *Heap<T>::get_data(){
-//	return this->store;
-//}
+template <class T>
+uint Heap<T>::right(const uint &i) {
+	return (i << 1) + 1;
+}
+
+template <class T>
+void Heap<T>::heapify(const uint &i) {
+	uint l = left(i);
+	uint r = right(i);
+
+	uint largest = i;
+
+	if (l <= heap_size && data[l] * coef > data[i] * coef)
+		largest = l;
+	if (r <= heap_size && data[r] * coef > data[largest] * coef)
+		largest = r;
+
+	if (largest != i) {
+		std::swap(data[i], data[largest]);
+		heapify(largest);
+	}
+}
+
+template <class T>
+void Heap<T>::build_heap() {
+	for (uint i = lenght / 2; i >= INDEX_ROOT; --i)
+		heapify(i);
+}
+
+template <class T>
+void Heap<T>::swap(const uint &i, const uint &j) {
+	T tmp = this->data[i];
+	this->data[i] = this->data[j];
+	this->data[j] = tmp;
+}
+
+template <class T>
+void Heap<T>::decrease_size() {
+	--this->heap_size;
+}
+
+template <class T>
+void Heap<T>::increase_size() {
+	++this->heap_size;
+}
+
+template <class T>
+uint Heap<T>::get_coef(){
+	return this->coef;
+}
+
+template <class T>
+uint Heap<T>::get_size(){
+	return this->heap_size;
+}
+
+template <class T>
+uint Heap<T>::get_lenght(){
+	return this->lenght;
+}
+
+template <class T>
+const T *Heap<T>::get_data(){
+	return this->data;
+}
+
+template Heap < int >;
+template Heap < float >;
+template Heap < double >;
+template Heap < char >;
+// ... add own data type

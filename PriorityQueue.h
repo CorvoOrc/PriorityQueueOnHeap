bed912eff518f4f2fcd939cefbe5c625ecacdfe7
Alexander Steshenko <CorvoOrc@users.noreply.github.com>
2015-01-25 00:01:19 +0400

    Update PriorityQueue.h
    Introduced separate compilation of templates

--- PriorityQueue.h
+++ PriorityQueue.h
@@ -15,59 +15,19 @@ heap_insert - O(lgn)
 #include <exception>
 
 template <class T>
-class PriorityQueue:public Heap<T> // How do separate compilation (use .cpp)?
+class PriorityQueue:public Heap<T>
 {
 public:
-	PriorityQueue(const Heap <T> &heap_):Heap<T>(heap_){
-	}
-	PriorityQueue(const uint &lenght_, const T *data_, const Type &type_):Heap<T>(lenght_, data_, type_){
-	}
-	~PriorityQueue() {
-		delete heap;
-	}
+	PriorityQueue(const Heap <T> &heap_);
+	PriorityQueue(const uint &lenght_, const T *data_, const Type &type_);
+	~PriorityQueue();
 
-	T heap_root() {
-		return data[INDEX_ROOT];
-	}
+	T heap_root();
 
-	T heap_extract_root(){
-		if (get_size() < MIN_HEAP_SIZE)
-			throw std::out_of_range("queue is empty");
+	T heap_extract_root();
 
-		T max = heap_root();
+	void heap_increase_key(uint i, const T &key);
 
-		data[INDEX_ROOT] = data[get_size()];
-		decrease_size();
-
-		uint index_root = INDEX_ROOT;
-		heapify(index_root);
-
-		return max;
-	}
-
-	void heap_increase_key(uint i, const int &key){
-		if (key < data[i])
-			throw std::runtime_error("new key less current");
-
-		data[i] = key;
-		for (; i > INDEX_ROOT && data[parent(i)] < data[i]; i = parent(i))
-			swap(i, parent(i));
-			//std::swap(data[i], data[parent(i)]);
-
-		if (get_coef() == -1)
-			build_heap();
-	}
-
-	void heap_insert(const int &key){
-		increase_size();
-		data[get_size()] = -INFINITY;
-
-		try{
-			heap_increase_key(get_size(), key);
-		}
-		catch (std::runtime_error &e) {
-			throw std::runtime_error(e.what());
-		}
-	}
+	void heap_insert(const T &key);
 };
 

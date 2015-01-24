6e192336b61ff2fec41c3513a05f6edda8518d45
Alexander Steshenko <CorvoOrc@users.noreply.github.com>
2015-01-25 00:00:30 +0400

    Update PriorityQueue.cpp
    Introduced separate compilation of templates

--- PriorityQueue.cpp
+++ PriorityQueue.cpp
@@ -1,55 +1,67 @@
 #include "PriorityQueue.h"
 
-//template <class T>
-//PriorityQueue<T>::PriorityQueue(const Heap<T> *heap_) {
-//	this->heap = new Heap<T>(*heap_);
-//}
-//
-//template <class T>
-//PriorityQueue<T>::~PriorityQueue()
-//{
-//}
+template <class T>
+PriorityQueue<T>::PriorityQueue(const Heap <T> &heap_):Heap<T>(heap_) {
+}
 
-//template <class T>
-//T PriorityQueue<T>::heap_root() {
-//	return heap->store[heap->INDEX_ROOT];
-//}
-//
-//template <class T>
-//T PriorityQueue<T>::heap_extract_root() {
-//	if (heap->get_size() < heap->MIN_HEAP_SIZE)
-//		throw std::out_of_range("queue is empty");
-//
-//	T max = heap_root();
-//	heap->store[heap->INDEX_ROOT] = heap->store[heap->get_size()];
-//	heap->decrease_size();
-//
-//	uint index_root = heap->INDEX_ROOT;
-//	heap->heapify(index_root);
-//
-//	return max;
-//}
-//
-//template <class T>
-//void PriorityQueue<T>::heap_increase_key(uint i, T &key) {
-//	if (key < heap->store[i])
-//		throw std::runtime_error("new key less current");
-//
-//	heap->store[i] = key;
-//	for (; i > heap->INDEX_ROOT && heap->store[heap->parent(i)] * heap->get_coef() < heap->store[i] * heap->get_coef(); i = heap->parent(i))
-//		std::swap(heap->store[i], heap->store[heap->parent(i)]);
-//}
-//
-//template <class T>
-//void PriorityQueue<T>::heap_insert(T &key) {
-//	heap->increase_size();
-//	uint i = heap->get_size();
-//	heap->store[i] = -INFINITY;
-//
-//	try{
-//		heap_increase_key(heap->heap_size, key);
-//	}
-//	catch (std::runtime_error &e) {
-//		throw std::runtime_error(e.what());
-//	}
-//}
+template <class T>
+PriorityQueue<T>::PriorityQueue(const uint &lenght_, const T *data_, const Type &type_):Heap<T>(lenght_, data_, type_) {
+}
+
+template <class T>
+PriorityQueue<T>::~PriorityQueue() {
+}
+
+template <class T>
+T PriorityQueue<T>::heap_root() {
+	return data[INDEX_ROOT];
+}
+
+template <class T>
+T PriorityQueue<T>::heap_extract_root() {
+	if (get_size() < MIN_HEAP_SIZE)
+		throw std::out_of_range("queue is empty");
+
+	T max = heap_root();
+
+	data[INDEX_ROOT] = data[get_size()];
+	decrease_size();
+
+	uint index_root = INDEX_ROOT;
+	heapify(index_root);
+
+	return max;
+}
+
+template <class T>
+void PriorityQueue<T>::heap_increase_key(uint i, const T &key) {
+	if (key < data[i])
+		throw std::runtime_error("new key less current");
+
+	data[i] = key;
+	for (; i > INDEX_ROOT && data[parent(i)] < data[i]; i = parent(i))
+		swap(i, parent(i));
+		//std::swap(data[i], data[parent(i)]);
+
+	if (get_coef() == -1)
+		build_heap();
+}
+
+template <class T>
+void PriorityQueue<T>::heap_insert(const T &key) {
+	increase_size();
+	data[get_size()] = -INFINITY;
+
+	try{
+		heap_increase_key(get_size(), key);
+	}
+	catch (std::runtime_error &e) {
+		throw std::runtime_error(e.what());
+	}
+}
+
+template PriorityQueue < int >;
+template PriorityQueue < float >;
+template PriorityQueue < double >;
+template PriorityQueue < char >;
+// ... add own data type

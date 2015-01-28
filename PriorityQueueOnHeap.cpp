242a07f73098dd2ce01db8bda84a0cf8e61b5f2a
Alexander Steshenko <CorvoOrc@users.noreply.github.com>
2015-01-28 15:59:36 +0400

    Update PriorityQueueOnHeap.cpp

--- PriorityQueueOnHeap.cpp
+++ PriorityQueueOnHeap.cpp
@@ -3,6 +3,7 @@
 This is my implementation Priority Queue on Heap.
 Theory take from book "Introduction to Algorithms" ,
 Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, and Clifford Stein
+(Sorting and Order Statistics - 6 Heapsort)
 */
 
 #include <iostream>
